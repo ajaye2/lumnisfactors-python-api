848f5119f0d5278dc665d6f6d4c87762253ce6b3
Abubakarr Jaye <ajaye2@illinois.edu>
2022-12-01 16:04:46 -0500

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lumnisfactors",
-    version="0.0.01",
+    version="0.1",
     author="Abubakarr Jaye",
     author_email="ajaye@lumnis.org",
     description="Lumnis Alternative Data",
